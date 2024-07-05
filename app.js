edbda7ba575db2f8c004a086afcbdfda8822fab1
misterpotabi <misterpotabi@gmail.com>
2024-07-05 19:46:25 +0700

    update lagi!

--- app.js
+++ app.js
@@ -6,14 +6,10 @@ const PORT = 8000
 const HOST = '0.0.0.0'
 
 const app = express()
-/*
-[ROUTE] '/'
-[Response] => Hello world!
- */
-app.get('/', (req, res) => {
-    res.send('Hello! ini saya Fajarhardeka-S1SI18092-Komgraf\n')
-})
 
+app.get('/', (req, res) => {
+    res.send('Hello! ini saya Fajarhardeka-S1SI18092-UAS-projek-komgraf\n')
+})
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
